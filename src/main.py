d51fac49ae55e4dd4d3eff905b3e3479734cc6b3
LudekCizinsky <ludekcizinsky11@gmail.com>
2022-03-27 15:46:48 +0200

    New code for eda part.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 # ---------------------- Spark setup
+from collections import defaultdict
+
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
@@ -29,7 +31,7 @@ def q2(bs):
     (name, stars, review count) 
     """
 
-    return bs.filter((bs.stars == 5) & (bs.review_count >= 1000))["name", "stars", "review_count"]
+    return bs.filter((bs.stars == 5) & (bs.review_count >= 1000)).select("name", "stars", "review_count")
 
 def q3(us):
     """
@@ -37,7 +39,7 @@ def q3(us):
     output should be in the form of DataFrame of user id.
     """
     
-    return us.filter(us.review_count > 1000)["user_id"]
+    return us.filter(us.review_count > 1000).select("user_id")
 
 
 def q4(bs, rs, a3):
@@ -63,6 +65,52 @@ def q5(rs, us):
     rs_us = rs.join(us, "user_id").groupby("user_id").mean("stars").sort("avg(stars)")
 
 
+# ---------------------- 3.2 Authenticity Study
+def aq1(bs, rs):
+    """
+    What is the percentage of reviews containing a variant of the word "authentic"?
+    How many reviews contain the string "legitimate" grouped by restaurant categories?
+    How many reviews contain the string "legitimate" grouped by businesses type (type of cuisine)?
+    """
+
+    # Get reviews for only restaurants
+    rest = bs[bs.categories.contains('Restaurants')]
+    rest_rs = rest.join(rs, "business_id")
+
+    # Get reviews with a form of the word authentic
+    rest_rs_auth = rest_rs.filter(rest_rs.text.rlike('authentic'))
+
+    # Get the percentage of such revirews
+    auth_perc = (rest_rs_auth.count()/rest_rs.count())*100
+    print(f">> Percentage of reviews with a form of authentic: {auth_perc} %\n")
+
+    # Get reviews with the word legitimate, and count them by the business category
+    rest_rs_legit = rest_rs.filter(rest_rs.text.rlike('legitimate')).collect()
+    bs_types_count = defaultdict(int)
+    for r in rest_rs_legit:
+        for c in r.categories.split(", "):
+            if c in ["Restaurants"]:
+                continue
+            else:
+                bs_types_count[c] += 1
+
+    # Sorted (decreasing order) list such that: [(category, count), ...]
+    bs_types_order = sorted([(t, c,) for t, c in bs_types_count.items()], key=lambda x: x[1], reverse=True)
+    print(">> Categories vs count for reviews including word 'legitimate':")
+    for cat, c in bs_types_order: print("≥≥≥ Category: {category:<35} : {count}".format(category=cat, count=c))
+    print()
+
+    # Normalize by the count in the actual category
+    bs_types_count_all = defaultdict(int)
+    rest_rs_rows = rest_rs.collect()
+    for r in rest_rs.collect():
+        for c in r.categories.split(", "):
+            if c in ["Restaurants"]:
+                continue
+            else:
+                bs_types_count_all[c] += 1
+
+
 def main():
 
     # ------------ 3.1 Specific DataFrame Queries
@@ -77,3 +125,7 @@ def main():
     a3 = q3(us)
     a4 = q4(bs, rs, a3)
     a5 = q5(rs, us)
+
+    # ------------ 3.2 Authenticity Study
+
+
